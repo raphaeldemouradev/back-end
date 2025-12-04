3653d747c8e77267e614da13ba31e477f0f1b3dd
Raphael de Moura <raphaelmoura.dev@gmail.com>
2025-12-04 12:57:18 -0300

    desistencia 1

--- node_js/pratica001/servidor.js
+++ node_js/pratica001/servidor.js
@@ -1,8 +1,33 @@
 const http = require('http');
+const fs = require('fs')
 
 http.createServer((req, res) => {
-    res.writeHead(200, {'content-type' : 'text/plain'});
+    res.writeHead(200, {'Content-Type' : 'text/html'});
+    
+    if (req.url === '/' && req.method === 'get') {
+        fs.readFile('index.html', (err, data) => {
+            res.writeHead(200, {'Content-Type' : 'text/html'});
+            res.end(data);
+        });
 
-    res.write('Hello World');
+    } else if (req.url === 'processar' && req.method == 'post') { //processar errado
+        let body = '';
+
+        req.on('data', chunk => {
+            body += chunk.toString();
+        })
+
+        req.on('end', () => {
+            const params = new URLSearchParams(body);
+            const nomeUser = params.get('nome');
+        })
+
+        fs.readFile('cadastro.html', 'utf8', (err, data) => {
+            let htmlNome = data.replace('<p id="user"> </p>', '<p>')
+        })
+    }
+
+
+    res.write(user);
     res.end();
 }).listen(3000);
