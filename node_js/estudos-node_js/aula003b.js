048119bd2ca6e28a9b241f86faca7ec8a0a9b861
Raphael de Moura <raphaelmoura.dev@gmail.com>
2025-12-04 23:54:15 -0300

    aula003b add

---
+++ node_js/estudos-node_js/aula003b.js
@@ -0,0 +1,21 @@
+const http = require('http');
+const url = require('url');
+const porta = 3000;
+const host = '127.0.0.1';
+
+const servidor = http.createServer((req, res) => {
+    res.writeHead(200, {'Content-Type' : 'text/html'});
+    res.write(req.url); //o que estiver na url vai para tela
+
+    const p = url.parse(req.url,true).query;
+    res.write('<br/>'+p.nome); //nome do User na url; exibe na tela
+    res.write('<br/>'+p.curso); //cuso da url; exibe na tela
+
+    res.end();
+})
+
+servidor.listen(porta, host, 
+    () => {console.log('Servidor rodando!')}
+);
+
+// Na url: http://localhost:3000/aula004b.js/nome=Alex&curso=node
