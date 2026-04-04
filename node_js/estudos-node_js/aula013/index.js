64a7f9aaf88a7a18373f94f55edb03abb16322dc
Raphael de Moura <raphaelmoura.dev@gmail.com>
2026-04-03 21:36:42 -0300

    aula013 add

--- node_js/estudos-node_js/aula013/index.js
+++ node_js/estudos-node_js/aula013/index.js
@@ -13,8 +13,28 @@ async function rodar() {
 
         const colecao = "curso"
 
+        /* INSERT
         const resultado = await dbo.collection(colecao).insertOne(obj)
         console.log("1 Curso inserido")
+        await banco.close()
+        */
+
+        /* Buscar Registros
+        // "findOne" mostra apenas 1 resgistro na busca. A " {} " manda procurar todos os registroas.
+        // "find" e "toArray" garante a busca de todos.
+        // Se colocar dentro das "{}" um valor do mongodb, ele retorna aquilo que esta igual lá, Exemplo: "canal: 'CFB Cursos'", se existir outo igual vai retornar tambem
+        // "projection" escode um registro do find. O 0 seguinifica coluna
+        const resultado = await dbo.collection(colecao).find({curso: 'Curso de Node'},{projection:{_id:0,}}).toArray()
+        console.log(resultado) // se colocar "[0]" traz o item da posição 0 e se colocar ".curso" traz somente o nome do curso
+        await banco.close()
+        */
+
+        // usando query
+        // se usar "/.t/" retorna os registros que termina com T
+        // se usar "/C./" retorna os registros que começa com C
+        const query = {curso: /.t/}
+        const resultado = await dbo.collection(colecao).find(query,{projection:{_id:0,}}).toArray()
+        console.log(resultado) 
         await banco.close()
 
     } catch (erro) {
