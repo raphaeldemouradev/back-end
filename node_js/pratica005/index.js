4f34616720a8ce59827b48f690e11ff6c4c8b3a8
Raphael de Moura <raphaelmoura.dev@gmail.com>
2026-04-20 03:22:34 -0300

    SQLite e incerir add

---
+++ node_js/pratica005/index.js
@@ -0,0 +1,3 @@
+import { prisma } from "./lib/prisma.js";
+import express from 'express';
+//# sourceMappingURL=index.js.map
---
+++ node_js/pratica005/index.ts
@@ -0,0 +1,64 @@
+import express from 'express';
+import path from 'path';
+import cors from 'cors';
+import { fileURLToPath } from 'url';
+import { prisma } from './lib/prisma.js';
+
+const app = express();
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+app.use(cors())
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+app.use(express.static(path.join(__dirname, 'public')))
+
+
+// Config Cadastro
+app.post('/cadastro', async (req, res) => {
+    // Gets Formulario
+    const { name, email, pass } = req.body;
+   
+    try {
+        // Create banco
+        const novoUsuario = await prisma.user.create({
+            data: { 
+                name: name, 
+                email: email, 
+                pass: pass 
+            }
+        })
+        console.log(novoUsuario)
+        
+        res.redirect(`/home`)
+
+    } catch (error) {
+        console.error("Erro de cadastro", error)
+        res.status(500).send("Erro no servidor ao salvar usuario")
+    }
+
+})
+
+// API call
+app.get('/api/users', async (req, res) => {
+    const usuarios = await prisma.user.findMany();
+    res.json(usuarios)
+})
+
+///// ROTAS ////
+// Cadastro
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/index.html'))
+})
+
+// Config Home
+app.get('/home', (req, res) => {
+    res.sendFile(path.join(__dirname, '/home.html'))
+})
+
+// Config Perfil
+app.get('/perfil', (req, res) => {
+    res.sendFile(path.join(__dirname, '/perfil.html'))
+})
+
+app.listen(3000, () => {console.log("Servidor On")})
